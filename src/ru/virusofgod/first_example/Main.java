13bc1f64bf17fe23ddd7c6812ac5ac095288a95e
Gulyaev Alexander <virusofgod@yandex.ru>
2022-10-19 14:17:07 +0400

    Lesson 19 First example of type casting finished

--- src/ru/virusofgod/first_example/Main.java
+++ src/ru/virusofgod/first_example/Main.java
@@ -1,4 +1,21 @@
 package ru.virusofgod.first_example;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Object o = new Scanner(System.in);
+        //((Scanner)o).nextInt();
+        Scanner scanner = null;
+        if (o instanceof Scanner) {
+            scanner = ((Scanner) o);
+        }
+
+        if (scanner != null){
+            scanner.nextInt();
+
+        }
+
+
+    }
 }
