c48b69419b7185e93ae6eab1f36ea71dc5ea10a5
Gulyaev Alexander <virusofgod@yandex.ru>
2022-11-27 22:35:15 +0400

    edited second_example

--- src/ru/virusofgod/second_example/Main.java
+++ src/ru/virusofgod/second_example/Main.java
@@ -1,4 +1,14 @@
 package ru.virusofgod.second_example;
 
 public class Main {
+    public static void main(String[] args) {
+        Object[]  objects = {10, "Привет", 3.14};// Здесь происходит автоупаковка
+        for(Object o : objects){
+            if(o instanceof String){
+                String s = (String) o;
+                System.out.println(s);
+            }
+
+        }
+    }
 }
