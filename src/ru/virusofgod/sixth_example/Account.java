b3054967021dc47d5b1e1771afa78ec6606d0b42
Gulyaev Alexander <virusofgod@yandex.ru>
2022-12-06 09:38:05 +0400

    added Account class

---
+++ src/ru/virusofgod/sixth_example/Account.java
@@ -0,0 +1,28 @@
+package ru.virusofgod.sixth_example;
+
+public class Account {
+
+    private int id;
+    private int sum;
+
+    public Account(int id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
--- src/ru/virusofgod/sixth_example/Main.java
+++ src/ru/virusofgod/sixth_example/Main.java
@@ -3,7 +3,7 @@ package ru.virusofgod.sixth_example;
 public class Main {
     public static void main(String[] args) {
 
-        
+
 
     }
 }
